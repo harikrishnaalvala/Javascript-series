4ab78e30f756ed57344030d57a9832bab39f3775
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-06-28 10:50:11 +0530

    Create index.js

---
+++ Chunk Array/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    a=[]
+    for (let i=0; i<arr.length;i+=size){
+        a.push(arr.slice(i,i+size));
+    }
+    return a
+};
